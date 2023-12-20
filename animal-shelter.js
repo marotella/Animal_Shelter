4400088f6185960a912cc08daad3013fed5c4465
marotella <michelle.a.rotella@gmail.com>
2023-12-19 20:41:03 -0500

    Step 1 compelted - Class created and tested

--- animal-shelter.js
+++ animal-shelter.js
@@ -1,1 +1,23 @@
 const animalData = require('./animal-data.json');
+
+class Animal {
+    constructor(name, species, color, hunger){
+        this.name = name;
+        this.species = species;
+        this.color = color;
+        this.hunger = hunger;
+    }
+    greet(){
+        console.log(`Hi! I'm ${this.name} the ${this.species}!`);
+    }
+    feed(){
+        this.hunger = this.hunger - 20;
+        console.log("Yum, I love food!");
+    }
+}
+
+const lou = new Animal ("Lou", "cat", "tuxedo", "1000");
+console.log(lou)
+lou.greet()
+lou.feed()
+console.log(lou)
